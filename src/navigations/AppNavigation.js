23dff71355639da87ad64e4563b63f3c315147e3
FatmaBetulUyar <fbluyar2@gmail.com>
2021-12-28 19:52:47 +0300

    Add proje files

--- src/navigations/AppNavigation.js
+++ src/navigations/AppNavigation.js
@@ -1,104 +1,100 @@
-import React, { useState} from 'react'
-import {Text,View} from 'react-native';
-import {createStackNavigator} from '@react-navigation/stack'
-import {NavigationContainer} from '@react-navigation/native'
-import {createDrawerNavigator} from '@react-navigation/drawer' 
-import DrawerContainer from "../screens/DrawerContainer/DrawerContainer"
-import PushNotification from '../screens/Notification/Notification';
-import LoginRegister from '../screens/LoginRegister/LoginRegister';
-import Jobs from '../screens/Jobs/Jobs';
-import JobDetails from '../screens/Details/Details'
-import { connectStorageEmulator } from '@firebase/storage';
-import Favorites from '../screens/Favorites/Favorites';
+import React, { useState } from "react";
+import { Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import DrawerContainer from "../screens/DrawerContainer/DrawerContainer";
+import PushNotification from "../screens/Notification/Notification";
+import LoginRegister from "../screens/LoginRegister/LoginRegister";
+import Jobs from "../screens/Jobs/Jobs";
+import JobDetails from "../screens/Details/Details";
+import { connectStorageEmulator } from "@firebase/storage";
+import Favorites from "../screens/Favorites/Favorites";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function MainNavigator() {
-  return(
-    <Stack.Navigator
-      screenOptions={{
+  
+
+  return (
+    <Stack.Navigator screenOptions={{headerTitleStyle: {fontWeight: "bold",textAlign: "center",alignSelf: "center",flex: 1, },}}>
+      <Stack.Screen name="Jobs" component={Jobs}
+        options={{
+          title: "Home",
+          headerStyle: { backgroundColor: "#FF6B6B" },
           headerTitleStyle: {
-            fontWeight: 'bold',
-            textAlign: 'center',
-            alignSelf: 'center',
-            flex: 1,
-          }
-      }}
-    >
-      <Stack.Screen name='Jobs' component={Jobs} options={{
-            title: "Home",
-            headerStyle: { backgroundColor: "#FF6B6B" },
-            headerTitleStyle: {
-              color: "white",
-              fontWeight: "bold",
-              fontSize: 25,
-            },
-            headerTitleAlign: "center",
-            headerTintColor: "white",
-          }}
- />
-        <Stack.Screen name='JobDetails' component={JobDetails} options={{
-            title: "Details",
-            headerStyle: { backgroundColor: "#FF6B6B" },
-            headerTitleStyle: {
-              color: "white",
-              fontWeight: "bold",
-              fontSize: 25,
-            },
-            headerTitleAlign: "center",
-            headerTintColor: "white",
-          }}
-/>
-<Stack.Screen name='Favorites' component={Favorites} options={{
-            title: "Favorites",
-            headerStyle: { backgroundColor: "#FF6B6B" },
-            headerTitleStyle: {
-              color: "white",
-              fontWeight: "bold",
-              fontSize: 25,
-            },
-            headerTitleAlign: "center",
-            headerTintColor: "white",
-          }}
-/>
-    
+            color: "white",
+            fontWeight: "bold",
+            fontSize: 25,
+          },
+          headerTitleAlign: "center",
+          headerTintColor: "white",
+        }}
+      />
+      <Stack.Screen name="JobDetails" component={JobDetails}
+        options={{
+          title: "Details",
+          headerStyle: { backgroundColor: "#FF6B6B" },
+          headerTitleStyle: {
+            color: "white",
+            fontWeight: "bold",
+            fontSize: 25,
+          },
+          headerTitleAlign: "center",
+          headerTintColor: "white",
+        }}
+      />
+      <Stack.Screen name="Favorites" component={Favorites}
+        options={{
+          title: "Favorites",
+          headerStyle: { backgroundColor: "#FF6B6B" },
+          headerTitleStyle: {
+            color: "white",
+            fontWeight: "bold",
+            fontSize: 25,
+          },
+          headerTitleAlign: "center",
+          headerTintColor: "white",
+        }}
+      />
     </Stack.Navigator>
-  )
-} 
+  );
+}
 
 const Drawer = createDrawerNavigator();
 
 function DrawerStack(props) {
-  const {setAuth} = props;
-  return(
+  const { setAuth } = props;
+  return (
     <Drawer.Navigator
-      drawerPosition='left'
-      initialRouteName='Main'
+      drawerPosition="left"
+      initialRouteName="Main"
       drawerStyle={{
-        width: 250
+        width: 250,
       }}
-      screenOptions={{headerShown: false}}
-      drawerContent={({navigation})=> <DrawerContainer setAuth={setAuth} navigation={navigation}/>}
+      screenOptions={{ headerShown: false }}
+      drawerContent={({ navigation }) => (
+        <DrawerContainer setAuth={setAuth} navigation={navigation} />
+      )}
     >
-  
-      <Drawer.Screen name='Main' component={MainNavigator} /> 
-     
-      
+      <Drawer.Screen name="Main" component={MainNavigator} />
     </Drawer.Navigator>
-  )
-} 
+  );
+}
 
- export default function AppContainer() {
-  const [auth, setAuth] = useState(false); 
+export default function AppContainer() {
+  const [auth, setAuth] = useState(false);
 
-  return(
-    auth == true 
-    ? <NavigationContainer>
-      <DrawerStack setAuth={setAuth}/>
+  return auth == true ? (
+    <NavigationContainer>
+      <DrawerStack setAuth={setAuth} />
     </NavigationContainer>
-    : <LoginRegister setAuth={setAuth}></LoginRegister>
-  )
-} 
- 
+  ) : (
+    <LoginRegister setAuth={setAuth}></LoginRegister>
+  );
+}
 
 console.disableYellowBox = true;
